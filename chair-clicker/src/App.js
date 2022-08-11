982e30f53a4b5d2b67a7b9f5bd36656a6d069640
Maximo Di napoli <maximodinapolimail@gmail.com>
2022-08-11 11:40:38 -0300

    Working

--- chair-clicker/src/App.js
+++ chair-clicker/src/App.js
@@ -1,53 +1,94 @@
 
 import './App.css';
 import Building from './components/Building';
-import Text from './components/Text';
+import Text from "./components/Text"
+import { useState } from 'react';
 
 function App() {
+
+  const [numChairs, setNumChairs] = useState(0); 
+
+  const [numBrokenChair, setNumBrokenChair] = useState(0); 
+  const [numGeneriChair, setNumGeneriChair] = useState(0); 
+  const [numSchoolChair, setNumSchoolChair] = useState(0); 
+  const [numMetalChair, setNumMetalChair] = useState(0); 
+  const [numGamerChair, setNumGamerChair] = useState(0); 
+  const [numGamerRgbChair, setNumGamerRgbChair] = useState(0); 
+
+  const brokenChairManagement = () => {
+    setNumBrokenChair(numBrokenChair + 1);
+  };
+
+  const generiChairManagement = () => {
+    setNumGeneriChair(numGeneriChair + 1);
+  };
+
+  const schoolChairManagement = () => {
+    setNumSchoolChair(numSchoolChair + 1);
+  };
+
+  const metalChairManagement = () => {
+    setNumMetalChair(numMetalChair + 1);
+  };
+
+  const gamerChairManagement = () => {
+    setNumGamerChair(numGamerChair + 1);
+  };
+
+  const gamerRgbManagement = () => {
+    setNumGamerRgbChair(numGamerRgbChair + 1);
+  };
+
+
   return (
     <div className="App">
       <div className='main-container'>
         <div className='data-container'>
-        <Text>123</Text>
+          <Text>123</Text>
         </div>
         <div className='buildings-container'>
 
           <Building img="lv1.png"
+            clickManagment={brokenChairManagement}
             alt="lv1Chair"
             buildingName="Broken-chair"
             buildingCost="10"
-            owned="0"/>
+            owned={numBrokenChair}/>
 
           <Building img="lv2.png"
-           alt="lv2Chair"
+            clickManagment={generiChairManagement}
+            alt="lv2Chair"
             buildingName="Generic-chair"
             buildingCost="100"
-            owned="0"/>
+            owned={numGeneriChair}/>
 
           <Building img="lv3.png"
-           alt="lv3Chair"
+            clickManagment={schoolChairManagement}
+            alt="lv3Chair"
             buildingName="School-chair"
             buildingCost="1000"
-            owned="0"/>
-
-
+            owned={numSchoolChair}/>
+          
           <Building img="lv4.png"
+            clickManagment={metalChairManagement}
             alt="lv4Chair"
             buildingName="Metal-chair"
             buildingCost="10000"
-            owned="0"/>
+            owned={numMetalChair}/>
 
           <Building img="lv5.png"
+            clickManagment={gamerChairManagement}
             alt="lv5Chair"
             buildingName="Gamer-chair"
             buildingCost="100000"
-            owned="0"/>
+            owned={numGamerChair}/>
 
           <Building img="lv6.png"
+            clickManagment={gamerRgbManagement}
             alt="lv6Chair"
             buildingName="Gamer-chair with rgb"
             buildingCost="1000000"
-            owned="0"/>
+            owned={numGamerRgbChair}/>
         
         </div>
       </div>

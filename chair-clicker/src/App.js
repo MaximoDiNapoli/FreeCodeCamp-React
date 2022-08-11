be508bc9b55ba12034cae53d2042cdf095cff3b6
Maximo Di napoli <maximodinapolimail@gmail.com>
2022-08-11 10:55:51 -0300

    Create chairs

--- chair-clicker/src/App.js
+++ chair-clicker/src/App.js
@@ -12,11 +12,41 @@ function App() {
         <div className='buildings-container'>
 
           <Building img="lv1.png"
-          alt="lv1Chair"
-          buildingName="Broken-chair"
-          buildingCost="10"
-          owned="0"/>
+            alt="lv1Chair"
+            buildingName="Broken-chair"
+            buildingCost="10"
+            owned="0"/>
 
+          <Building img="lv2.png"
+           alt="lv2Chair"
+            buildingName="Generic-chair"
+            buildingCost="100"
+            owned="0"/>
+
+          <Building img="lv3.png"
+           alt="lv3Chair"
+            buildingName="School-chair"
+            buildingCost="1000"
+            owned="0"/>
+
+
+          <Building img="lv4.png"
+            alt="lv4Chair"
+            buildingName="Metal-chair"
+            buildingCost="10000"
+            owned="0"/>
+
+          <Building img="lv5.png"
+            alt="lv5Chair"
+            buildingName="Gamer-chair"
+            buildingCost="100000"
+            owned="0"/>
+
+          <Building img="lv6.png"
+            alt="lv6Chair"
+            buildingName="Gamer-chair with rgb"
+            buildingCost="1000000"
+            owned="0"/>
         
         </div>
       </div>
